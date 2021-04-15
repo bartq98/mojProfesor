8e4ac5d30c014255b8daa8befd2492cba92afa9b
JacobChwastek <jacobchwastek@gmail.com>
2021-04-15 22:02:00 +0200

    Test component view and first search input styling

--- frontend/src/components/common/Input/SearchInput.tsx
+++ frontend/src/components/common/Input/SearchInput.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Input } from 'antd';
+import styled from 'styled-components';
+
+const { Search } = Input;
 
 type Props = {};
 
-const SearchInput: React.FC<Props> = () => <div>Test</div>;
+const StyledSearch = styled(Search)`
+    input {
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+    button {
+        border-bottom-right-radius: 20px;
+        border-top-right-radius: 20px;
+    }
+`;
+
+const SearchInput: React.FC<Props> = () => {
+    const onSearch = () => {};
+
+    return (
+        <StyledSearch
+            placeholder="input search text"
+            onSearch={onSearch}
+            enterButton
+        />
+    );
+};
 
 export default SearchInput;
---
+++ frontend/src/views/ComponentsView.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Space } from 'antd';
+import { Input } from 'components/common';
+import styled from 'styled-components';
+
+const { SearchInput } = Input;
+
+const Container = styled.div`
+    display: flex;
+    width: 100vw;
+    min-height: 100vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ComponentsView: React.FC = () => (
+    <Container>
+        <Space direction="horizontal">
+            <SearchInput />
+        </Space>
+    </Container>
+);
+
+export default ComponentsView;
