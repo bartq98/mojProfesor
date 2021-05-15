b9f0e917c46da94f640b735710f45a6a9cbd422c
Natalia Bidzińska <natalia.b9910@gmail.com>
2021-05-15 20:15:34 +0200

    Small improvements

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -6,21 +6,6 @@ import 'assets/css/Login.css';
 const RegisterView: React.FC = () => (
     <div className="App">
         <Register />
-        {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit 
-                <code> src/App.tsx </code>
-                and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer">
-                Learn Reaaaaaact
-            </a>
-        </header> */}
     </div>
 );
 
